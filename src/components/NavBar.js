72ce69202fcec8c4f916edfc50bae066fd376ce4
Greg <gvasquezencalada@gmail.com>
2024-01-02 15:42:01 -0800

    removed navbar temporarily to keep from breaking site

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,58 +1,68 @@
-import React, { useState } from 'react';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText,
-} from 'reactstrap';
+import React from 'react'
 
-function NavBar(args) {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggle = () => setIsOpen(!isOpen);
-
+const NavBar = () => {
   return (
-    <div>
-      <Navbar {...args}>
-        <NavbarBrand href="/">Welcome!</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="me-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">About Me</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">
-                Projects
-              </NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Socials
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>GitHub</DropdownItem>
-                <DropdownItem>LinkedIn</DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>Reset</DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-          <NavbarText></NavbarText>
-        </Collapse>
-      </Navbar>
-    </div>
-  );
+    <div>NavBar</div>
+  )
 }
+
+export default NavBar
+
+// import React, { useState } from 'react';
+// import {
+//   Collapse,
+//   Navbar,
+//   NavbarToggler,
+//   NavbarBrand,
+//   Nav,
+//   NavItem,
+//   NavLink,
+//   UncontrolledDropdown,
+//   DropdownToggle,
+//   DropdownMenu,
+//   DropdownItem,
+//   NavbarText,
+// } from 'reactstrap';
+
+// function NavBar(args) {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const toggle = () => setIsOpen(!isOpen);
+
+//   return (
+//     <div>
+//       <Navbar {...args}>
+//         <NavbarBrand href="/">Welcome!</NavbarBrand>
+//         <NavbarToggler onClick={toggle} />
+//         <Collapse isOpen={isOpen} navbar>
+//           <Nav className="me-auto" navbar>
+//             <NavItem>
+//               <NavLink href="/components/">About Me</NavLink>
+//             </NavItem>
+//             <NavItem>
+//               <NavLink href="https://github.com/reactstrap/reactstrap">
+//                 Projects
+//               </NavLink>
+//             </NavItem>
+//             <UncontrolledDropdown nav inNavbar>
+//               <DropdownToggle nav caret>
+//                 Socials
+//               </DropdownToggle>
+//               <DropdownMenu right>
+//                 <DropdownItem>GitHub</DropdownItem>
+//                 <DropdownItem>LinkedIn</DropdownItem>
+//                 <DropdownItem divider />
+//                 <DropdownItem>Reset</DropdownItem>
+//               </DropdownMenu>
+//             </UncontrolledDropdown>
+//           </Nav>
+//           <NavbarText></NavbarText>
+//         </Collapse>
+//       </Navbar>
+//     </div>
+//   );
+// }
     
   
 
-export default NavBar
+// export default NavBar
