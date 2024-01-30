104c5a43735dc37c0a00fd6203ebbb6d24dc51b6
Greg <gvasquezencalada@gmail.com>
2024-01-29 16:51:04 -0800

    set up basic component rendering (need to set up navbar tabs which seem to be broken)

--- src/App.js
+++ src/App.js
@@ -5,19 +5,23 @@ import ContactMe from './components/ContactMe'
 import './App.css'
 import NavBar from './components/NavBar'
 import Greeter from './components/Greeter'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+
 
 const App = () => {
   return (
-    <>
-      <div className='overall-site-border'>
+    <div className='overall-site-border'>
+      <Router>
         <NavBar />
-        <Greeter />
-        <AboutMe/>
-        <Projects/>
-        <ContactMe/>
-      </div>
-    </>
-  )
-}
+        <Routes>
+          <Route path="/" element={<Greeter />} />
+          <Route path="/aboutme" element={<AboutMe />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="contactme" element={<ContactMe />} />
+        </Routes>
+        {/* <Footer /> */}
+      </Router>
+    </div>
+  )}
 
 export default App
