6d14f40c2ab97843bc91f141bae4a9f0243df535
Greg <gvasquezencalada@gmail.com>
2024-02-01 12:09:02 -0800

    fixed zoom transition issues caused by padding and height differences between components and added light styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,10 @@ import React from 'react'
 const Home = () => {
     return(
         <div className='greeter'>
-            <h1>Hello There! Welcome to </h1>
+            <h3>Hello There! I'm </h3>
             <h1 className='padding'>Gregory Encalada</h1>
-            <p />
-            <h2 className='padding'>"The difference between a novice and a master is that the master has failed more times than the novice has tried"</h2>
+            <h3>A Full-Stack Developer</h3>
+            <p className='padding'>"The difference between a novice and a master is that the master has failed more times than the novice has tried"</p>
         </div>
     )
 }
